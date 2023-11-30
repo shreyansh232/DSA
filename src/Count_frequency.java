aeee21b09500c8800c55aaeb8e21b25e8252030b
shreyansh232 <shreyansh487@gmail.com>
2023-11-30 15:08:03 +0530

    First Problem

--- src/Count_frequency.java
+++ src/Count_frequency.java
@@ -1,26 +1,48 @@
 import java.util.*;
 public class Count_frequency {
+//    public static void main(String[] args) {
+//        int[] arr = {10, 5, 10, 3, 4, 4, 10};
+//        int n = arr.length;
+//        countFrequency(arr, n);
+//    }
+//    public static void countFrequency(int[] arr, int n){
+//        boolean[] visited = new boolean[n];
+//        for(int i = 0; i < n; i++ ){
+//
+//            if(visited[i] == true){
+//                continue;
+//            }
+//
+//            int count = 1;
+//            for(int j = i +1; j < n; j++){
+//                if(arr[i] == arr[j]){
+//                    visited[j] = true;
+//                    count++;
+//                }
+//            }
+//            System.out.println(arr[i] + " " + count);
+//        }
+//
+//    }
+
+
+    //USING HASHMAP
     public static void main(String[] args) {
         int[] arr = {10, 5, 10, 3, 4, 4, 10};
         int n = arr.length;
         countFrequency(arr, n);
     }
     public static void countFrequency(int[] arr, int n){
-        boolean[] visited = new boolean[n];
-        for(int i = 0; i < n; i++ ){
-
-            if(visited[i] == true){
-                continue;
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < n; i++){
+            if (map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i]) + 1);
+            }else{
+                map.put(arr[i], 1);
             }
-
-            int count = 1;
-            for(int j = i +1; j < n; j++){
-                if(arr[i] == arr[j]){
-                    visited[j] = true;
-                    count++;
-                }
-            }
-            System.out.println(arr[i] + " " + count);
+        }
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()){
+            System.out.println(entry.getKey() + " " + entry.getValue());
         }
 
     }

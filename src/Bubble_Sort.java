a99aa27e42cfe34cbef340af9919d37070232cb1
shreyansh232 <shreyansh487@gmail.com>
2023-12-04 22:40:22 +0530

    Bubble Sort

--- src/Bubble_Sort.java
+++ src/Bubble_Sort.java
@@ -1,7 +1,6 @@
 public class Bubble_Sort {
     public static void bubbleSort(int[] arr, int n) {
         for (int i = n - 1; i > 0; i--) {
-
             for (int j = 0; j < i ; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j+1];

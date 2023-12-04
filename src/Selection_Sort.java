5b1f6398805177b5cb5071a272a005bcb362dffa
shreyansh232 <shreyansh487@gmail.com>
2023-12-04 22:41:51 +0530

    Bubble Sort

--- src/Selection_Sort.java
+++ src/Selection_Sort.java
@@ -18,7 +18,6 @@ public class Selection_Sort{
     }
 
     public static void main(String[] args) {
-
         int[] arr = {13, 46, 24, 52, 20, 9};
         int n = arr.length;
         selection_sort(arr, n);

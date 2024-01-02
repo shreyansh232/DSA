5f1b95d76892dc9d5612b7836a32741d881d234b
shreyansh232 <shreyansh487@gmail.com>
2024-01-02 20:17:41 +0530

    Merge Sort

--- src/Insertion_Sort.java
+++ src/Insertion_Sort.java
@@ -1,24 +1,50 @@
+//public class Insertion_Sort {
+//    public static void insertionSort(int[] arr, int n){
+//        for (int i = 0; i < n; i++){
+//            int j = i;
+//            while (j>0 && arr[j-1] > arr[j]){
+//                int temp = arr[j-1];
+//                arr[j - 1] = arr[j];
+//                arr[j] = temp;
+//                j--;
+//            }
+//        }
+//        System.out.println("After insertion sort: ");
+//        for (int i = 0; i < n; i++){
+//            System.out.print(arr[i] + " ");
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        int[] arr = {13, 46, 24, 52, 20, 9};
+//        int n = arr.length;
+//        insertionSort(arr, n);
+//    }
+//
+//}
+
 public class Insertion_Sort {
-    public static void insertionSort(int[] arr, int n){
-        for (int i = 0; i < n; i++){
-            int j = i;
-            while (j>0 && arr[j-1] > arr[j]){
-                int temp = arr[j-1];
-                arr[j - 1] = arr[j];
-                arr[j] = temp;
-                j--;
-            }
+    public static void insertionSort(int[] arr, int i, int n){
+        if (i == n) return;
+        int j = i;
+        while(j>0 && arr[j-1] > arr[j]){
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
         }
-        System.out.println("After insertion sort: ");
-        for (int i = 0; i < n; i++){
-            System.out.print(arr[i] + " ");
-        }
+        insertionSort(arr, i+1, n);
     }
 
     public static void main(String[] args) {
         int[] arr = {13, 46, 24, 52, 20, 9};
         int n = arr.length;
-        insertionSort(arr, n);
-    }
+        insertionSort(arr, 0, n);
+        System.out.println("After insertion sort: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
 
+    }
 }

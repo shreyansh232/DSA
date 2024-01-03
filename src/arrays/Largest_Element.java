e2e9d686a43c02d9d61649b4cfc16ddc81c76ead
shreyansh232 <shreyansh487@gmail.com>
2024-01-03 21:01:05 +0530

    Merge Sort

---
+++ src/arrays/Largest_Element.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class Largest_Element {
+    public static void main(String[] args) {
+        int[] arr = {3,1,4,5,6,9,2};
+        largest(arr);
+
+    }
+    static void largest(int[] arr){
+        int largest = arr[0];
+        for (int i = 0; i< arr.length - 1; i++){
+            if (arr[i]>largest){
+                largest = arr[i];
+            }
+        }
+        System.out.println(largest);
+    }
+
+}

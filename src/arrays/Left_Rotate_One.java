bff09920308d5b098612c801b1bdb4707eac89d0
shreyansh232 <shreyansh487@gmail.com>
2024-01-05 22:05:02 +0530

    Merge Sort

--- src/arrays/Left_Rotate_One.java
+++ src/arrays/Left_Rotate_One.java
@@ -8,7 +8,9 @@ public class Left_Rotate_One {
 
     }
     static void rotate(int[] arr, int n){
+
         int temp = arr[0];
+
         for (int i = 1; i < n ; i++ ){
             arr[i-1] = arr[i];
 

c807c2a6cb947456d09facb26ee0c3abb975975e
shreyansh232 <shreyansh487@gmail.com>
2024-01-09 22:50:03 +0530

    DSA

--- src/arrays/Missing_Number.java
+++ src/arrays/Missing_Number.java
@@ -26,7 +26,8 @@ public class Missing_Number {
 //        int ans = missingNumber(arr, N);
 //        System.out.println("The missing number is: " + ans);
 //    }
-    public static int missingNumber(int[] a, int N){int xor1 = 0, xor2 = 0;
+    public static int missingNumber(int[] a, int N){
+        int xor1 = 0, xor2 = 0;
         for (int i = 0; i < N-1; i++){
             xor2 = xor2^a[i];
             xor1 = xor1^(i+1);

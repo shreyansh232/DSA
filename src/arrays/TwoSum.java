284b39ebc487aca54c47c1797541c49391147c19
shreyansh232 <shreyansh487@gmail.com>
2024-01-13 10:59:12 +0530

    DSA

--- src/arrays/TwoSum.java
+++ src/arrays/TwoSum.java
@@ -1,20 +1,30 @@
 package arrays;
 
 public class TwoSum {
-    public static int twoSum(int[] nums, int target){
+    public static int[] twoSum(int[] nums, int target) {
         int n = nums.length;
-        for (int i = 0; i < n; i++){
-            for (int j = 0; j < n; j++){
-                if (i == j){
-                    continue;
-                }
-                if (nums[i] + nums[j] == target){
-                    return nums[i] + nums[j];
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    // Return an array containing the indices of the pair
+                    return new int[]{i, j};
                 }
             }
         }
-        // You might want to handle the case where no such pair is found
-        // by returning a specific value or throwing an exception.
-        return -1;  // or throw new IllegalArgumentException("No such pair found");
+        // Return an array indicating that no such pair is found
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 3, 5, 1, 9};
+        int target = 10;
+        int[] indices = twoSum(nums, target);
+
+        // Check if a valid pair is found
+        if (indices[0] != -1) {
+            System.out.println("Indices of the pair: " + indices[0] + ", " + indices[1]);
+        } else {
+            System.out.println("No such pair found.");
+        }
     }
 }

dc0001d267c258e680abe7f1f829dde0a5ccbd6c
shreyansh232 <shreyansh487@gmail.com>
2024-01-14 16:35:06 +0530

    DSA

--- src/arrays/Second_Largest.java
+++ src/arrays/Second_Largest.java
@@ -28,12 +28,18 @@ public class Second_Largest {
 
     }
     static void secondLargest(int[] arr) {
+        int n = arr.length;
         int largest = arr[0];
-        int sL = -1;
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] > largest) {
+        for(int i = 1; i < n; i++){
+            if(arr[i] > largest){
                 largest = arr[i];
-                sL = arr[i - 1];
+            }
+
+        }
+        int sL = -1;
+        for(int i = 0; i < n; i++){
+            if(arr[i] > sL && arr[i] != largest){
+                sL = arr[i];
             }
         }
         System.out.println(sL);

e38230eb12b0fff2d5fd35b76c2488998427a8f5
shreyansh232 <shreyansh487@gmail.com>
2024-01-18 22:06:45 +0530

    DSA

--- src/arrays/sort012.java
+++ src/arrays/sort012.java
@@ -1,7 +1,10 @@
 package arrays;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class sort012 {
-    public void sortColors(int[] nums) {
+    public static void sortColors(int[] nums) {
         int count1 = 0, count2 = 0, count3 = 0;
         for (int i : nums) {
             if (i == 0) {
@@ -16,6 +19,17 @@ public class sort012 {
         for (int i = 0; i < count1; i++) nums[i] = 0;
         for (int i = count1; i < count1 + count2; i++) nums[i] = 1;
         for (int i = count1 + count2; i < count1 + count2 + count3; i++) nums[i] = 2;
+    }
+    public static void main(String args[]) {
+        int n = 6;
+        int[] arr = {0, 2, 1, 2, 0, 1};
+        sortColors(arr);
+        System.out.println("After sorting:");
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i]);
+        }
+        System.out.println();
+
     }
 
 }

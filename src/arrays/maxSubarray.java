6b5cf9d771324ddd785e68eadd45fdc13398cdb8
shreyansh232 <shreyansh487@gmail.com>
2024-01-20 02:30:25 +0530

    DSA

--- src/arrays/maxSubarray.java
+++ src/arrays/maxSubarray.java
@@ -15,14 +15,38 @@ public class maxSubarray {
 //        }
 //        return maxi;
         //Better
+//        int maxi = Integer.MIN_VALUE; // maximum sum
+//        for(int i = 0; i < n; i++){
+//                int sum = 0;
+//            for(int j = i; j < n; j++){
+//                    sum = sum + arr[j];
+//                maxi = Math.max(maxi, sum);
+//            }
+//        }
+//        return maxi;
+
         int maxi = Integer.MIN_VALUE; // maximum sum
-        for(int i = 0; i < n; i++){
-                int sum = 0;
-            for(int j = i; j < n; j++){
-                    sum = sum + arr[j];
-                maxi = Math.max(maxi, sum);
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            sum += arr[i];
+
+            if (sum > maxi) {
+                maxi = sum;
+            }
+
+            // If sum < 0: discard the sum calculated
+            if (sum < 0) {
+                sum = 0;
             }
         }
+
+        // To consider the sum of the empty subarray
+        // uncomment the following check:
+
+        //if (maxi < 0) maxi = 0;
+
         return maxi;
 
     }

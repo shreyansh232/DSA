eb38f0e339777383f0b0665e9b348840636791a2
shreyansh232 <shreyansh487@gmail.com>
2024-01-19 16:57:17 +0530

    DSA

--- src/arrays/sort012.java
+++ src/arrays/sort012.java
@@ -3,21 +3,46 @@ package arrays;
 public class sort012 {
 
     //Better
+//    public static void sortColors(int[] nums) {
+//        int count1 = 0, count2 = 0, count3 = 0;
+//        for (int i : nums) {
+//            if (i == 0) {
+//                count1++;
+//            } else if (i == 1) {
+//                count2++;
+//            } else {
+//                count3++;
+//            }
+//        }
+//
+//        for (int i = 0; i < count1; i++) nums[i] = 0;
+//        for (int i = count1; i < count1 + count2; i++) nums[i] = 1;
+//        for (int i = count1 + count2; i < count1 + count2 + count3; i++) nums[i] = 2;
+//    }
+
+    //Optimal
     public static void sortColors(int[] nums) {
-        int count1 = 0, count2 = 0, count3 = 0;
+
+
+        int low = 0, mid = 0, high = nums.length - 1;
         for (int i : nums) {
-            if (i == 0) {
-                count1++;
-            } else if (i == 1) {
-                count2++;
+            if (nums[mid] == 0) {
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+
+            } else if (nums[mid] == 1) {
+                mid++;
             } else {
-                count3++;
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+
             }
         }
-
-        for (int i = 0; i < count1; i++) nums[i] = 0;
-        for (int i = count1; i < count1 + count2; i++) nums[i] = 1;
-        for (int i = count1 + count2; i < count1 + count2 + count3; i++) nums[i] = 2;
     }
     public static void main(String[] args) {
         int n = 6;

598e6edd08f6ee3386de7bdcb8982b6db8681213
shreyansh232 <shreyansh487@gmail.com>
2024-01-23 21:54:42 +0530

    DSA

--- src/arrays/setMismatch.java
+++ src/arrays/setMismatch.java
@@ -1,5 +1,59 @@
 package arrays;
 
 public class setMismatch {
+    public int[] findErrorNums(int[] nums) {
+        // int[] arr = new int[2];
+        // HashSet<Integer> set = new HashSet<Integer>();
+        // int n = nums.length;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (!set.add(nums[i])) {
+        //         arr[0] = nums[i];
+        //     }
+        // }
+
+
+        // for (int i = 1; i <= n; i++) {
+        //     if (!set.contains(i)) {
+
+        //         arr[1] = i;
+        //         break;
+        //     }
+        // }
+
+        // return arr;
+        int i =0;
+
+        int[] arr = new int[2];
+
+        while(i<nums.length){
+
+            int correct = nums[i] - 1;
+
+            if(nums[i] != nums[correct]){
+                swap(nums, i, correct);
+            }
+            else{
+                i++;
+            }
+        }
+
+        for(int j=0; j<nums.length; j++){
+
+            if(nums[j] != j+1){
+                arr[0] = nums[j];
+                arr[1] = j+1;
+            }
+        }
+
+        return arr;
+
+    }
+
+    void swap(int[] arr, int first, int second){
+        int temp = arr[ first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
 
 }

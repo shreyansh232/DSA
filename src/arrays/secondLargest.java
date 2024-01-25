c38e79ce2f6d0545929e47f8dbfa9abd64a27f5f
shreyansh232 <shreyansh487@gmail.com>
2024-01-25 17:19:14 +0530

    DSA

--- src/arrays/secondLargest.java
+++ src/arrays/secondLargest.java
@@ -2,7 +2,7 @@ package arrays;
 
 import java.util.Arrays;
 
-public class Second_Largest {
+public class secondLargest {
 //    public static void main(String[] args) {
 //        int[] arr = {3,1,4,5,6,9,2};
 //        secondLargest(arr);

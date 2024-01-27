7104e8dc1481ee9cdac76de2f0c78649e000268b
shreyansh232 <shreyansh487@gmail.com>
2024-01-27 23:57:17 +0530

    DSA

--- src/arrays/rotateMatrix90.java
+++ src/arrays/rotateMatrix90.java
@@ -1,28 +1,62 @@
 package arrays;
 
 public class rotateMatrix90 {
-    static int[][] rotate(int[][] matrix) {
-        int n = matrix.length;
-        int rotated[][] = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                rotated[j][n - i - 1] = matrix[i][j];
+//    static int[][] rotate(int[][] matrix) {
+//        int n = matrix.length;
+//        int rotated[][] = new int[n][n];
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < n; j++) {
+//                rotated[j][n - i - 1] = matrix[i][j];
+//            }
+//        }
+//        return rotated;
+//    }
+//
+//    public static void main(String args[]) {
+//        int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+//        int rotated[][] = rotate(arr);
+//        System.out.println("Rotated Image");
+//        for (int i = 0; i < rotated.length; i++) {
+//            for (int j = 0; j < rotated.length; j++) {
+//                System.out.print(rotated[i][j] + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//    }
+
+    static void rotate(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = i; j < matrix[0].length; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        return rotated;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length / 2; j++) {
+                int temp = 0;
+                temp = matrix[i][j];
+                matrix[i][j] = matrix[i][matrix.length - 1 - j];
+                matrix[i][matrix.length - 1 - j] = temp;
+            }
+        }
     }
 
     public static void main(String args[]) {
         int arr[][] =  {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        int rotated[][] = rotate(arr);
+        rotate(arr);
         System.out.println("Rotated Image");
-        for (int i = 0; i < rotated.length; i++) {
-            for (int j = 0; j < rotated.length; j++) {
-                System.out.print(rotated[i][j] + " ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
 
     }
+
+
 
 }

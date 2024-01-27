3f5dbcbdbad70bc6af0c457fa60ce09dea752d19
shreyansh232 <shreyansh487@gmail.com>
2024-01-27 23:33:46 +0530

    DSA

--- src/arrays/setMatrixZeroes.java
+++ src/arrays/setMatrixZeroes.java
@@ -3,63 +3,149 @@ package arrays;
 import java.util.*;
 
 public class setMatrixZeroes {
-    static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
-        // set all non-zero elements as -1 in the row i:
-        for (int j = 0; j < m; j++) {
-            if (matrix.get(i).get(j) != 0) {
-                matrix.get(i).set(j, -1);
-            }
-        }
-    }
+    //Brute
+//    static void markRow(ArrayList<ArrayList<Integer>> matrix, int n, int m, int i) {
+//        // set all non-zero elements as -1 in the row i:
+//        for (int j = 0; j < m; j++) {
+//            if (matrix.get(i).get(j) != 0) {
+//                matrix.get(i).set(j, -1);
+//            }
+//        }
+//    }
+//
+//    static void markCol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j) {
+//        // set all non-zero elements as -1 in the col j:
+//        for (int i = 0; i < n; i++) {
+//            if (matrix.get(i).get(j) != 0) {
+//                matrix.get(i).set(j, -1);
+//            }
+//        }
+//    }
+//
+//    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+//        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (matrix.get(i).get(j) == 0) {
+//                    markRow(matrix, n, m, i);
+//                    markCol(matrix, n, m, j);
+//                }
+//            }
+//        }
+//        // Finally, mark all -1 as 0:
+//        for (int i = 0; i < n; i++) {
+//            for (int j = 0; j < m; j++) {
+//                if (matrix.get(i).get(j) == -1) {
+//                    matrix.get(i).set(j, 0);
+//                }
+//            }
+//        }
+//        return matrix;
+//    }
+    //Better
+//    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
+//        int[] row = new int[n];
+//        int[] col = new int[m];
+//
+//        for(int i = 0; i < n; i++){
+//            for (int j = 0; j < m; j++){
+//                if(matrix.get(i).get(j) == 0){
+//                    row[i] = 1;
+//                    col[j] = 1;
+//                }
+//            }
+//        }
+//
+//        for(int i = 0; i < n; i++){
+//            for(int j = 0; j < m; j++){
+//                if(row[i] == 1 || col[j] == 1){
+//                    matrix.get(i).set(j, 0);
+//                }
+//            }
+//        }
+//        return matrix;
+//    }
+//
+//    public static void main(String[] args) {
+//        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
+//        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+//        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
+//        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+//
+//        int n = matrix.size();
+//        int m = matrix.get(0).size();
+//
+//        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+//
+//        System.out.println("The Final matrix is: ");
+//        for (ArrayList<Integer> row : ans) {
+//            for (Integer ele : row) {
+//                System.out.print(ele + " ");
+//            }
+//            System.out.println();
+//        }
+//    }
+    //Optimal
+    public static void setZeroes(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
 
-    static void markCol(ArrayList<ArrayList<Integer>> matrix, int n, int m, int j) {
-        // set all non-zero elements as -1 in the col j:
-        for (int i = 0; i < n; i++) {
-            if (matrix.get(i).get(j) != 0) {
-                matrix.get(i).set(j, -1);
-            }
-        }
-    }
+        int col0 = 1;
 
-    static ArrayList<ArrayList<Integer>> zeroMatrix(ArrayList<ArrayList<Integer>> matrix, int n, int m) {
-        // Set -1 for rows and cols that contains 0. Don't mark any 0 as -1:
+        // Step 1: Traverse the matrix and mark 1st row & col accordingly
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (matrix.get(i).get(j) == 0) {
-                    markRow(matrix, n, m, i);
-                    markCol(matrix, n, m, j);
+                if (matrix[i][j] == 0) {
+                    // Mark i-th row
+                    matrix[i][0] = 0;
+
+                    // Mark j-th column
+                    if (j != 0)
+                        matrix[0][j] = 0;
+                    else
+                        col0 = 0;
                 }
             }
         }
-        // Finally, mark all -1 as 0:
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (matrix.get(i).get(j) == -1) {
-                    matrix.get(i).set(j, 0);
+
+        // Step 2: Mark with 0 from (1, 1) to (n-1, m-1)
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (matrix[i][j] != 0) {
+                    // Check for col & row
+                    if (matrix[i][0] == 0 || matrix[0][j] == 0) {
+                        matrix[i][j] = 0;
+                    }
                 }
             }
         }
-        return matrix;
-    }
 
+        // Step 3: Finally, mark the 1st col & then 1st row
+        if (matrix[0][0] == 0) {
+            for (int j = 0; j < m; j++) {
+                matrix[0][j] = 0;
+            }
+        }
+        if (col0 == 0) {
+            for (int i = 0; i < n; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
     public static void main(String[] args) {
-        ArrayList<ArrayList<Integer>> matrix = new ArrayList<>();
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 0, 1)));
-        matrix.add(new ArrayList<>(Arrays.asList(1, 1, 1)));
+        int[][] matrix = {
+                {1, 1, 1},
+                {1, 0, 1},
+                {1, 1, 1}
+        };
 
-        int n = matrix.size();
-        int m = matrix.get(0).size();
-
-        ArrayList<ArrayList<Integer>> ans = zeroMatrix(matrix, n, m);
+        setZeroes(matrix);
 
         System.out.println("The Final matrix is: ");
-        for (ArrayList<Integer> row : ans) {
-            for (Integer ele : row) {
-                System.out.print(ele + " ");
-            }
-            System.out.println();
+        for (int[] row : matrix) {
+            System.out.println(Arrays.toString(row));
         }
     }
+
 
 }

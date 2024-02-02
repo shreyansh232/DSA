234332b77d20a289b9f2ca6b101d1f27458088d5
shreyansh232 <shreyansh487@gmail.com>
2024-02-02 22:56:29 +0530

    DSA

---
+++ src/arrays/PascalTriangle.java
@@ -0,0 +1,25 @@
+package arrays;
+
+import java.util.*;
+
+public class PascalTriangle {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                // Calculate binomial coefficient C(i, j) using dynamic programming
+                int coef = (j == 0 || j == i) ? 1 : row.get(j - 1) * (i - j + 1) / j;
+                row.add(coef);
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+
+    
+}

a82e1ab4e57e7682b38c28be5c38fc27c461dbd5
shreyansh232 <shreyansh487@gmail.com>
2024-02-07 17:55:42 +0530

    DSA

--- src/arrays/PascalTriangle.java
+++ src/arrays/PascalTriangle.java
@@ -3,25 +3,46 @@ package arrays;
 import java.util.*;
 
 public class PascalTriangle {
+//    public List<List<Integer>> generate(int numRows) {
+//        List<List<Integer>> result = new ArrayList<>();
+//
+//        for (int i = 0; i < numRows; i++) {
+//            List<Integer> row = new ArrayList<>();
+//
+//            for (int j = 0; j <= i; j++) {
+//                // Calculate binomial coefficient C(i, j) using dynamic programming
+//                int coef = (j == 0 || j == i) ? 1 : row.get(j - 1) * (i - j + 1) / j;
+//                row.add(coef);
+//            }
+//
+//            result.add(row);
+//        }
+//
+//        return result;
+//
+//
+//
+//    }
+
     public List<List<Integer>> generate(int numRows) {
-        List<List<Integer>> result = new ArrayList<>();
-
-        for (int i = 0; i < numRows; i++) {
+        List<List<Integer>> triangle = new ArrayList<>();
+        if(numRows == 0) return triangle;
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+        for(int i = 1; i < numRows; i++){
+            List<Integer> prevRow = triangle.get(i-1);
             List<Integer> row = new ArrayList<>();
-
-            for (int j = 0; j <= i; j++) {
-                // Calculate binomial coefficient C(i, j) using dynamic programming
-                int coef = (j == 0 || j == i) ? 1 : row.get(j - 1) * (i - j + 1) / j;
-                row.add(coef);
+            row.add(1);
+            for(int j = 1; j < i; j++){
+                row.add(prevRow.get(j-1) + prevRow.get(j));
             }
+            row.add(1);
+            triangle.add(row);
 
-            result.add(row);
+
         }
-
-        return result;
-
-
-
+        return triangle;
     }
 
     

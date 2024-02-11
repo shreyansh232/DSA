6fce4e8d9ba48e19794c7403b53f95d180084ce6
shreyansh232 <shreyansh487@gmail.com>
2024-02-11 23:58:58 +0530

    DSA

--- src/arrays/MajorityElementnBy3.java
+++ src/arrays/MajorityElementnBy3.java
@@ -4,46 +4,46 @@ import java.util.*;
 public class MajorityElementnBy3 {
 
     //comitting this yee haw.....
-    public static List<Integer> majorityElement(int []v) {
-        int n = v.length; //size of the array
-        List<Integer> ls = new ArrayList<>(); // list of answers
-
-
-
-        for (int i = 0; i < n; i++) {
-            //selected element is v[i]:
-            // Checking if v[i] is not already
-            // a part of the answer:
-            if (ls.isEmpty() || ls.get(0) != v[i]) {
-                int cnt = 0;
-                for (int j = 0; j < n; j++) {
-                    // counting the frequency of v[i]
-                    if (v[j] == v[i]) {
-                        cnt++;
-
-                    }
-                }
-
-
-
-                // check if frquency is greater than n/3:
-                if (cnt > (n / 3))
-                    ls.add(v[i]);
-            }
-
-            if (ls.size() == 2) break;
-        }
-
-        return ls;
-    }
-
-    public static void main(String args[]) {
-        int[] arr = {11, 33, 33, 11, 33, 11};
-        List<Integer> ans = majorityElement(arr);
-        System.out.print("The majority elements are: ");
-        for (int i = 0; i < ans.size(); i++) {
-            System.out.print(ans.get(i) + " ");
-        }
+//    public static List<Integer> majorityElement(int []v) {
+//        int n = v.length; //size of the array
+//        List<Integer> ls = new ArrayList<>(); // list of answers
+//
+//
+//
+//        for (int i = 0; i < n; i++) {
+//            //selected element is v[i]:
+//            // Checking if v[i] is not already
+//            // a part of the answer:
+//            if (ls.isEmpty() || ls.get(0) != v[i]) {
+//                int cnt = 0;
+//                for (int j = 0; j < n; j++) {
+//                    // counting the frequency of v[i]
+//                    if (v[j] == v[i]) {
+//                        cnt++;
+//
+//                    }
+//                }
+//
+//
+//
+//                // check if frquency is greater than n/3:
+//                if (cnt > (n / 3))
+//                    ls.add(v[i]);
+//            }
+//
+//            if (ls.size() == 2) break;
+//        }
+//
+//        return ls;
+//    }
+//
+//    public static void main(String args[]) {
+//        int[] arr = {11, 33, 33, 11, 33, 11};
+//        List<Integer> ans = majorityElement(arr);
+//        System.out.print("The majority elements are: ");
+//        for (int i = 0; i < ans.size(); i++) {
+//            System.out.print(ans.get(i) + " ");
+//        }
         System.out.println();
     }
 

e45b46f269db50e7d6f9ede5cab8aea505442f4d
shreyansh232 <shreyansh487@gmail.com>
2024-02-12 00:42:31 +0530

    DSA

--- src/arrays/MajorityElementnBy3.java
+++ src/arrays/MajorityElementnBy3.java
@@ -70,6 +70,46 @@ public class MajorityElementnBy3 {
 //
 //        return ls;
 //    }
+    class Solution {
+        public List<Integer> majorityElement(int[] nums) {
+            int n = nums.length;
+            int cnt1 = 0, cnt2 = 0;
+            int ele1 = Integer.MIN_VALUE;
+            int ele2 = Integer.MIN_VALUE;
+
+            // First pass to find potential candidates
+            for(int i = 0; i < n; i++){
+                if(cnt1 == 0 && ele2 != nums[i]){
+                    cnt1 = 1;
+                    ele1 = nums[i];
+                }
+                else if(cnt2 == 0 && ele1 != nums[i]){
+                    cnt2 = 1;
+                    ele2 = nums[i];
+                }
+                else if(ele1 == nums[i]) cnt1++;
+                else if(ele2 == nums[i]) cnt2++;
+                else{
+                    cnt1--;
+                    cnt2--;
+                }
+            }
+
+            // Second pass to count occurrences
+            cnt1 = 0; cnt2 = 0;
+            for(int i = 0; i < n; i++){
+                if(nums[i] == ele1) cnt1++;
+                else if(nums[i] == ele2) cnt2++;
+            }
+
+            List<Integer> result = new ArrayList<>();
+            int min = (int)(n/3)+1;
+            if (cnt1 >= min) result.add(ele1);
+            if (cnt2 >= min) result.add(ele2);
+
+            return result;
+        }
+    }
 
 
     public static void main(String args[]) {

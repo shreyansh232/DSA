12db4876c6af42b00d3b06a8094f77c7e214a19a
shreyansh232 <shreyansh487@gmail.com>
2024-02-12 00:42:16 +0530

    DSA

--- src/arrays/MajorityElementnBy3.java
+++ src/arrays/MajorityElementnBy3.java
@@ -45,31 +45,32 @@ public class MajorityElementnBy3 {
 //            System.out.print(ans.get(i) + " ");
 //        }
 //        System.out.println();
-    public static List<Integer> majorityElement(int []v) {
-        int n = v.length; //size of the array
-        List<Integer> ls = new ArrayList<>(); // list of answers
+//    public static List<Integer> majorityElement(int []v) {
+//        int n = v.length; //size of the array
+//        List<Integer> ls = new ArrayList<>(); // list of answers
+//
+//        //declaring a map:
+//        HashMap<Integer, Integer> mpp = new HashMap<>();
+//
+//        // least occurrence of the majority element:
+//        int mini = (int)(n / 3) + 1;
+//
+//        //storing the elements with its occurnce:
+//        for (int i = 0; i < n; i++) {
+//            int value = mpp.getOrDefault(v[i], 0);
+//            mpp.put(v[i], value + 1);
+//
+//            //checking if v[i] is
+//            // the majority element:
+//            if (mpp.get(v[i]) == mini) {
+//                ls.add(v[i]);
+//            }
+//            if (ls.size() == 2) break;
+//        }
+//
+//        return ls;
+//    }
 
-        //declaring a map:
-        HashMap<Integer, Integer> mpp = new HashMap<>();
-
-        // least occurrence of the majority element:
-        int mini = (int)(n / 3) + 1;
-
-        //storing the elements with its occurnce:
-        for (int i = 0; i < n; i++) {
-            int value = mpp.getOrDefault(v[i], 0);
-            mpp.put(v[i], value + 1);
-
-            //checking if v[i] is
-            // the majority element:
-            if (mpp.get(v[i]) == mini) {
-                ls.add(v[i]);
-            }
-            if (ls.size() == 2) break;
-        }
-
-        return ls;
-    }
 
     public static void main(String args[]) {
         int[] arr = {11, 33, 33, 11, 33, 11};

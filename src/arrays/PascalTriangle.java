b9959e4e4038117c3d80e570329e21d4d81349a5
shreyansh232 <shreyansh487@gmail.com>
2024-02-11 23:15:21 +0530

    DSA

--- src/arrays/PascalTriangle.java
+++ src/arrays/PascalTriangle.java
@@ -45,26 +45,26 @@ public class PascalTriangle {
 //        return triangle;
 //    }
 
-    public static List<Integer> generateRow(int row) {
-        long ans = 1;
-        List<Integer> ansRow = new ArrayList<>();
-        ansRow.add(1); //inserting the 1st element
-
-        //calculate the rest of the elements:
-        for (int col = 1; col < row; col++) {
-            ans = ans * (row - col);
-            ans = ans / col;
-            ansRow.add((int)ans);
+    public static int nCr(int n, int r) {
+        long res = 1;
+        // calculating nCr:
+        for (int i = 0; i < r; i++) {
+            res = res * (n - i);
+            res = res / (i + 1);
         }
-        return ansRow;
+        return (int) res;
     }
 
     public static List<List<Integer>> pascalTriangle(int n) {
         List<List<Integer>> ans = new ArrayList<>();
 
-        //store the entire pascal's triangle:
+        // Store the entire Pascal's triangle:
         for (int row = 1; row <= n; row++) {
-            ans.add(generateRow(row));
+            List<Integer> tempLst = new ArrayList<>(); // temporary list
+            for (int col = 1; col <= row; col++) {
+                tempLst.add(nCr(row - 1, col - 1));
+            }
+            ans.add(tempLst);
         }
         return ans;
     }

5dd9d9d90a2b5b3a2db6536f47f671dc4ba351df
shreyansh232 <shreyansh487@gmail.com>
2024-02-15 23:17:15 +0530

    DSA

---
+++ src/arrays/PolygonPerimeter.java
@@ -0,0 +1,25 @@
+package arrays;
+import java.util.*;
+
+public class PolygonPerimeter {
+
+        public long largestPerimeter(int[] nums) {
+            Arrays.sort(nums);
+            int n = nums.length;
+            long sum = 0;
+            for (int num : nums) {
+                sum += num;
+            }
+            for (int i = n - 1; i >= 2; i--) {
+                if (sum - nums[i] > nums[i]) {
+                    return sum;
+                }
+                sum -= nums[i];
+            }
+            return -1;
+        }
+
+    public static void main(String[] args) {
+
+    }
+}

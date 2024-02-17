897be85d781f13e658af2bc4032208981677218b
shreyansh232 <shreyansh487@gmail.com>
2024-02-17 18:55:33 +0530

    DSA

--- src/arrays/fourSum.java
+++ src/arrays/fourSum.java
@@ -2,33 +2,45 @@ package arrays;
 import java.util.*;
 
 public class fourSum {
-    public static List<List<Integer>> fourSum(int[] nums, int target) {
-        Set<List<Integer>> st = new HashSet<>();
-        int n = nums.length;
+    public static List<List<Integer>> foursum(int[] nums, int target) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
 
-        // check all possible triplets:
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                for (int k = j + 1; k < n; k++) {
-                    for(int a = k + 1; a < n; a++){
-                        if (nums[i] + nums[j] + nums[k] + nums[a] == target) {
-                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[a]);
-                            temp.sort(null);
-                            st.add(temp);
-                        }
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < nums.length; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int k = j + 1;
+                int l = nums.length - 1;
+
+                while (k < l) {
+                    long sum = (long) nums[i] + nums[j] + nums[k] + nums[l];
+
+                    if (sum == target) {
+                        ans.add(Arrays.asList(nums[i], nums[j], nums[k], nums[l]));
+
+                        k++;
+                        l--;
+                        while (k < l && nums[k] == nums[k - 1]) k++;
+                        while (k < l && nums[l] == nums[l + 1]) l--;
+
+                    } else if (sum < target) {
+                        k++;
+                    } else {
+                        l--;
                     }
                 }
             }
         }
 
-        List<List<Integer>> ans = new ArrayList<>(st);
         return ans;
-
     }
     public static void main(String[] args) {
         int[] arr = { -1, 0, 1, 2, -1, -4};
         int target = 0;
-        List<List<Integer>> ans = fourSum(arr, target);
+        List<List<Integer>> ans = foursum(arr, target);
         for (List<Integer> it : ans) {
             System.out.print("[");
             for (Integer i : it) {

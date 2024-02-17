82dfb9c63ff37d3c879d0d3039882d0e8330088c
shreyansh232 <shreyansh487@gmail.com>
2024-02-17 16:51:39 +0530

    DSA

--- src/arrays/ThreeSum.java
+++ src/arrays/ThreeSum.java
@@ -23,29 +23,32 @@ public class ThreeSum {
 //        List<List<Integer>> ans = new ArrayList<>(st);
 //        return ans;
 //    }
-    public static List<List<Integer>> triplet(int n, int[] arr) {
-        Set<List<Integer>> st = new HashSet<>();
 
-        for (int i = 0; i < n; i++) {
-            Set<Integer> hashset = new HashSet<>();
-            for (int j = i + 1; j < n; j++) {
-                //Calculate the 3rd element:
-                int third = -(arr[i] + arr[j]);
 
-                //Find the element in the set:
-                if (hashset.contains(third)) {
-                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
-                    temp.sort(null);
-                    st.add(temp);
-                }
-                hashset.add(arr[j]);
-            }
-        }
-
-        // store the set elements in the answer:
-        List<List<Integer>> ans = new ArrayList<>(st);
-        return ans;
-    }
+    //Better
+//    public static List<List<Integer>> triplet(int n, int[] arr) {
+//        Set<List<Integer>> st = new HashSet<>();
+//
+//        for (int i = 0; i < n; i++) {
+//            Set<Integer> hashset = new HashSet<>();
+//            for (int j = i + 1; j < n; j++) {
+//                //Calculate the 3rd element:
+//                int third = -(arr[i] + arr[j]);
+//
+//                //Find the element in the set:
+//                if (hashset.contains(third)) {
+//                    List<Integer> temp = Arrays.asList(arr[i], arr[j], third);
+//                    temp.sort(null);
+//                    st.add(temp);
+//                }
+//                hashset.add(arr[j]);
+//            }
+//        }
+//
+//        // store the set elements in the answer:
+//        List<List<Integer>> ans = new ArrayList<>(st);
+//        return ans;
+//    }
 
     public static void main(String[] args) {
         int[] arr = { -1, 0, 1, 2, -1, -4};

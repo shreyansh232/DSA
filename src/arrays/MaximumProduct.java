d9ddc9d22693db7a6384dfca96f2b1b4b3cb4264
shreyansh232 <shreyansh487@gmail.com>
2024-02-23 23:25:32 +0530

    DSA

--- src/arrays/MaximumProduct.java
+++ src/arrays/MaximumProduct.java
@@ -1,5 +1,19 @@
 package arrays;
 
 public class MaximumProduct {
+    public static int maxProduct(int[] nums) {
+        int n = nums.length;
+        int res = Integer.MIN_VALUE;
+        int pre = 1;
+        int suf = 1;
+        for (int i = 0; i < n; i++) {
+            if (pre == 0) pre = 1;
+            if (suf == 0) suf = 1;
+            pre = pre * nums[i];
+            suf = suf * nums[n - i - 1];
+            res = Math.max(res, Math.max(pre, suf));
+        }
+        return res;
+    }
 
 }

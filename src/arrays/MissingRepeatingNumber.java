55d42c826bd7c1d180a1153d660013f642cbca32
shreyansh232 <shreyansh487@gmail.com>
2024-02-23 15:18:03 +0530

    DSA

--- src/arrays/MissingRepeatingNumber.java
+++ src/arrays/MissingRepeatingNumber.java
@@ -1,5 +1,20 @@
 package arrays;
 
 public class MissingRepeatingNumber {
+    public static int[] findMissingRepeatingNumbers(int []a) {
+        int n = a.length; // size of the array
+        int[] hash = new int[n + 1];
+        for(int i = 0; i < n; i++){
+            hash[a[i]]++;
+        }
+        int repeating = -1, missing = -1;
+        for(int i = 1; i <= n; i++){
+            if(hash[i] == 2) repeating = i;
+            else if (hash[i] == 0) missing = i;
+            if(repeating != -1 && missing != -1) break;
+        }
+        int[] ans = {repeating, missing};
+        return ans;
+    }
 
 }

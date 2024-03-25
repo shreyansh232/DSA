8b04f35429b42acd02be96c2f1f273a13260e0d4
shreyansh232 <shreyansh487@gmail.com>
2024-03-25 18:08:15 +0530

    DSA

---
+++ src/arrays/ClearIthBit.java
@@ -0,0 +1,15 @@
+package arrays;
+
+public class ClearIthBit {
+    static int clearKthBit(int n, int k) {
+        // Use bitwise AND with the complement of the bit mask for the k-th bit
+        return n & ~(1 << k);
+    }
+
+    public static void main(String[] args) {
+        int n = 13;
+        int k = 2;
+        int result = clearKthBit(n, k);
+        System.out.println(result);
+    }
+}

b3330639e0e9df6dded5510bcbe695194f7de630
shreyansh232 <shreyansh487@gmail.com>
2024-04-05 23:34:24 +0530

    DSA

---
+++ src/Strings/PS1614.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class PS1614 {
+    public int maxDepth(String s) {
+        int count=0,max=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(') {
+                count++;
+            }
+            max=Math.max(count,max);
+            if(s.charAt(i)==')') {
+                count--;
+            }
+        }
+        return max;
+    }
+
+
+}

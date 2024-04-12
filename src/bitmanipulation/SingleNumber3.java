45884eeb1f94fc8e8477a773fa54a72b34766dc6
shreyansh232 <shreyansh487@gmail.com>
2024-04-12 17:16:28 +0530

    DSA

--- src/bitmanipulation/SingleNumber3.java
+++ src/bitmanipulation/SingleNumber3.java
@@ -1,17 +1,15 @@
 package bitmanipulation;
+import java.util.Arrays;
+
 
 public class SingleNumber3 {
-
-
-    public int[] singleNumber(int[] nums) {
+    public static int[] singleNumber(int[] nums) {
         long xor = 0;
         for (int i = 0; i < nums.length; i++) {
             xor = xor ^ nums[i];
         }
-
         // Find the rightmost set bit of xor
         int rightmostSetBit = (int) (xor & -xor); // Equivalent to xor & (~xor + 1)
-
         int b1 = 0, b2 = 0;
         for (int num : nums) {
             if ((num & rightmostSetBit) != 0) {
@@ -24,5 +22,9 @@ public class SingleNumber3 {
         return new int[]{b1, b2};
     }
 
-
+    public static void main(String[] args) {
+        int[] nums = {1,2,1,3,2,5};
+        int[] result = singleNumber(nums);
+        System.out.println(Arrays.toString(result));
+    }
 }

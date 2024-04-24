516206ff1f430284b3b19bd0b8774dd73f601ec4
shreyansh232 <shreyansh487@gmail.com>
2024-04-24 13:45:27 +0530

    DSA

---
+++ src/arrays/PeakElement.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class PeakElement {
+    public static int findPeakElement(int[] arr) {
+        int n = arr.length;
+        if(n == 1) return 0;
+        if(arr[0] > arr[1]) return 0;
+        if(arr[n - 1] > arr[n-2]) return n-1;
+        int low = 1;
+        int high = n - 2;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+
+            if(arr[mid] > arr[mid + 1] && arr[mid] > arr[mid - 1]) return mid;
+            if(arr[mid] > arr[mid - 1]) low = mid + 1;
+            else{
+                high = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,8,2,1};
+        System.out.println(findPeakElement(arr));
+    }
+}

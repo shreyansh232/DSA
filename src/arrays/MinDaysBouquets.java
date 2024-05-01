3e6858fc4ac4dc023dbab5c8fa65f2533c6dc57b
shreyansh232 <shreyansh487@gmail.com>
2024-05-01 11:56:41 +0530

    DSA

--- src/arrays/MinDaysBouquets.java
+++ src/arrays/MinDaysBouquets.java
@@ -20,6 +20,24 @@ public class MinDaysBouquets {
 
 
     public static int minDays(int[] nums, int m, int k) {
+
+
+        //Brute Force:
+//        int max = Integer.MIN_VALUE;
+//        int min = Integer.MAX_VALUE;
+//        for(int i = 0; i < nums.length; i++){
+//            max = Math.max(max, nums[i]);
+//            min = Math.min(min, nums[i]);
+//        }
+//        for(int i = min; i <= max; i++){
+//            if(possible(nums, i, m, k) == true){
+//                return i;
+//            }
+//        }
+//        return -1;
+
+
+
         long val = (long) m*k;
         if(val > nums.length){
             return -1;

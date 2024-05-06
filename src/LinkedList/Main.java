1bed91c43d519a6aa6b2e1ce867ac1e23b6c24bc
shreyansh232 <shreyansh487@gmail.com>
2024-05-06 23:41:37 +0530

    DSA

--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -3,5 +3,12 @@ package LinkedList;
 public class Main {
     public static void main(String[] args) {
         LL list = new LL();
+        list.insertFirst(10);
+        list.insertFirst(3);
+        list.insertFirst(5);
+        list.insertFirst(9);
+        list.insertFirst(4);
+
+        list.display();
     }
 }

51f1b326bd0aad3eb202402d3dcb01fc1a1d4aaa
Shreyansh Pandey <shreyansh487@gmail.com>
2024-07-01 02:14:29 +0530

    Striver SDE Sheet - 02

---
+++ src/StriverSDESheet/PascalTriangle.java
@@ -0,0 +1,31 @@
+package StriverSDESheet;
+import java.util.*;
+
+public class PascalTriangle {
+    // TC - O(n*n) SC - 0(n*n)
+    public static List<List<Integer>> triangle(int n){
+        List<List<Integer>> ans  = new ArrayList<>();
+        for(int row = 1; row < n; row++){
+            ans.add(generateRows(row));
+        }
+        return ans;
+
+    }
+    public static List<Integer> generateRows(int row){
+        List<Integer> rows = new ArrayList<>();
+        long res = 1;
+        for(int i = 0; i < row; i++){
+            res = res * (row - i);
+            res = res / (i + 1);
+            rows.add((int)res);
+        }
+        return rows;
+
+    }
+
+    public static void main(String[] args) {
+        int n = 5;
+        System.out.print(triangle(n));
+    }
+
+}

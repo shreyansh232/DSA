4fb6c3a808f4567f34b2e5359c7fda366448ff2f
Shreyansh Pandey <shreyansh487@gmail.com>
2024-07-17 20:23:45 +0530

    Binary Subaray Sum

--- src/BinaryTree/LowestCommonAncestor.java
+++ src/BinaryTree/LowestCommonAncestor.java
@@ -1,23 +1,23 @@
-package BinaryTree;
-
-import javax.swing.tree.TreeNode;
-
-public class LowestCommonAncestor {
-    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q){
-            return root;
-        }
-        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
-        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
-        if(leftNode == null){
-            return rightNode;
-        } else if(rightNode == null){
-            return leftNode;
-        }else{
-            return root;
-        }
-
-
-    }
-
-}
+//package BinaryTree;
+//
+//import javax.swing.tree.TreeNode;
+//
+//public class LowestCommonAncestor {
+//    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//        if(root == null || root == p || root == q){
+//            return root;
+//        }
+//        TreeNode leftNode = lowestCommonAncestor(root.left, p, q);
+//        TreeNode rightNode = lowestCommonAncestor(root.right, p, q);
+//        if(leftNode == null){
+//            return rightNode;
+//        } else if(rightNode == null){
+//            return leftNode;
+//        }else{
+//            return root;
+//        }
+//
+//
+//    }
+//
+//}

d904dea1251b1e9978bfaa985cdcca727606f527
shreyansh-web <shreyansh487@gmail.com>
2024-08-07 22:22:40 +0530

    POTD

---
+++ src/Strings/IntegerToEnglishWords.java
@@ -0,0 +1,44 @@
+package Strings;
+
+public class IntegerToEnglishWords {
+    private static String[] belowTen = { "", "One", "Two", "Three", "Four", "Five", "Six",
+            "Seven", "Eight", "Nine" };
+    private static String[] belowTwenty = { "Ten", "Eleven", "Twelve", "Thirteen",
+            "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen",
+            "Nineteen" };
+
+    private static String[] belowHundred = { "", "Ten", "Twenty", "Thirty", "Forty", "Fifty", "Sixty",
+            "Seventy", "Eighty", "Ninety" };
+
+    public static String numberToWords(int num) {
+        if (num == 0)
+            return "Zero";
+        if (num < 10) {
+            return belowTen[num];
+        }
+        if (num < 20) {
+            return belowTwenty[num - 10];
+        }
+        if (num < 100) { // 74, 70
+            return belowHundred[num / 10] + (num % 10 != 0 ? " " + belowTen[num % 10] : "");
+        }
+        if (num < 1000) { // 9 99
+            return belowTen[num / 100] + " Hundred" + (num % 100 != 0 ? " " + numberToWords(num % 100) : "");
+        }
+        if (num < 1000000) { // 9000 - 999 000
+            return numberToWords(num / 1000) + " Thousand" + (num % 1000 != 0 ? " " + numberToWords(num % 1000) : "");
+        }
+        if (num < 1000000000) { // 9 99
+            return numberToWords(num / 1000000) + " Million"
+                    + (num % 1000000 != 0 ? " " + numberToWords(num % 1000000) : "");
+        }
+        return numberToWords(num / 1000000000) + " Billion"
+                + (num % 1000000000 != 0 ? " " + numberToWords(num % 1000000000) : "");
+
+    }
+
+    public static void main(String[] args) {
+        int num = 123;
+        System.out.println(numberToWords(num));
+    }
+}
